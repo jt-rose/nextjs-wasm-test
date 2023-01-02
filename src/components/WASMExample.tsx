7530fc338abd22c531f8e9033ca995014841693d
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-01-01 23:05:29 -0500

    test user input with wasm

--- src/components/WASMExample.tsx
+++ src/components/WASMExample.tsx
@@ -1,12 +1,40 @@
-import { useContext } from "react"
-import { WASMContext } from "../context/WASM"
+import { useState, useContext } from "react";
+import { WASMContext } from "../context/WASM";
 
 export const WASMExample = () => {
-  const ctx = useContext(WASMContext)
+  const ctx = useContext(WASMContext);
 
   if (!ctx.wasm) {
-    return <>...</>
+    return <>...</>;
   }
 
-  return <>Computed from WASM: 4+3={ctx.wasm.add(4,3)}</>
-}
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState<number | undefined>(0);
+
+  const addNums = () => {
+    setResult(ctx.wasm?.add(num1, num2));
+  };
+
+  // return <>Computed from WASM: 4+3={ctx.wasm.add(4,3)}</>
+  return (
+    <div>
+      <input
+        type="number"
+        name="num1"
+        id="num1"
+        value={num1}
+        onChange={(e) => setNum1(Number(e.target.value))}
+      />
+      <input
+        type="number"
+        name="num2"
+        id="num2"
+        value={num2}
+        onChange={(e) => setNum2(Number(e.target.value))}
+      />
+      <button onClick={addNums}>Add</button>
+      <h2>The result is: {result}</h2>
+    </div>
+  );
+};
